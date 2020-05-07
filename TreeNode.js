731b58fb09a8b18be27b1b9c61ede81cdd733aa1
m-gaston <35315240+m-gaston@users.noreply.github.com>
2020-05-07 17:38:19 -0400

    Added some files

--- TreeNode.js
+++ TreeNode.js
@@ -6,41 +6,6 @@ var TreeNode = /** @class */ (function () {
         this.token = token;
         this.children = [];
     }
-<<<<<<< Updated upstream
-    TreeNode.prototype.toString = function () {
-        function walk(n, callback) {
-            callback(n);
-            n.children.forEach(function (x) {
-                walk(x, callback);
-            });
-        }
-        var L = [];
-        L.push("digraph d{");
-        L.push("node [fontname=\"Courier New\", shape=box];");
-        var counter = 0;
-        walk(this, function (n) {
-            n.NUMBER = n.sym + (counter++);
-            var tmp = n.sym;
-            if (n.token) {
-                tmp += "\n";
-                tmp += n.token.lexeme;
-            }
-            tmp = tmp.replace(/&/g, "&amp;");
-            tmp = tmp.replace(/</g, "&lt;");
-            tmp = tmp.replace(/>/g, "&gt;");
-            tmp = tmp.replace(/\n/g, "<br/>");
-            L.push(n.NUMBER + " [label=<" + tmp + ">];");
-        });
-        walk(this, function (n) {
-            n.children.forEach(function (x) {
-                L.push(n.NUMBER + " -> " + x.NUMBER + ";");
-            });
-        });
-        L.push("}");
-        return L.join("\n");
-    };
-=======
->>>>>>> Stashed changes
     return TreeNode;
 }());
 exports.TreeNode = TreeNode;
---
+++ testharness.js
@@ -0,0 +1,105 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var shuntingyard_1 = require("./shuntingyard");
+var testCount = 0;
+function main() {
+    var ok = testWithFile("basictests.txt");
+    if (ok)
+        process.stderr.write("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Basic tests OK-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    else
+        return;
+    ok = testWithFile("bonus1tests.txt");
+    if (ok)
+        process.stderr.write("-=-=-=-=-=-=-=-=-=-=-Bonus 1 tests (1+ argument functions) OK-=-=-=-=-=-=-=-=-=-=-\n");
+    else
+        return;
+    ok = testWithFile("bonus2tests.txt");
+    if (ok)
+        process.stderr.write("-=-=-=-=-=-=-=-=-=-=-Bonus 2 tests (0+ argument functions) OK-=-=-=-=-=-=-=-=-=-=-\n");
+    else
+        return;
+    process.stderr.write(testCount + " tests OK\n");
+}
+function testWithFile(fname) {
+    var data = fs.readFileSync(fname, "utf8");
+    var lst = data.split(/\n/g);
+    for (var i = 0; i < lst.length; ++i) {
+        var line = lst[i].trim();
+        if (line.length === 0)
+            continue;
+        var idx = line.indexOf("\t");
+        var inp = line.substring(0, idx);
+        var expectedStr = line.substring(idx);
+        console.log("Testing " + inp + " ...");
+        ++testCount;
+        var expected = JSON.parse(expectedStr);
+        var actual = shuntingyard_1.parse(inp);
+        if (!treesAreSame(actual, expected)) {
+            console.log("BAD!");
+            console.log(inp);
+            dumpTree("actual.dot", actual);
+            dumpTree("expected.dot", expected);
+            return false;
+        }
+        else {
+        }
+    }
+    return true;
+}
+function treesAreSame(n1, n2) {
+    if (n1 === undefined && n2 !== undefined)
+        return false;
+    if (n2 === undefined && n1 !== undefined)
+        return false;
+    if (n1["sym"] != n2["sym"])
+        return false;
+    if (n1["children"].length != n2["children"].length)
+        return false;
+    for (var i = 0; i < n1["children"].length; ++i) {
+        if (!treesAreSame(n1["children"][i], n2["children"][i]))
+            return false;
+    }
+    return true;
+}
+function dumpTree(fname, root) {
+    function walk(n, callback) {
+        if (!n)
+            return;
+        callback(n);
+        if (!n.children)
+            return;
+        n.children.forEach(function (x) {
+            if (!x)
+                return;
+            walk(x, callback);
+        });
+    }
+    var L = [];
+    L.push("digraph d{");
+    L.push("node [fontname=\"Helvetica\",shape=box];");
+    var counter = 0;
+    walk(root, function (n) {
+        if (!n)
+            return;
+        n.NUMBER = "n" + (counter++);
+        var tmp = n.sym;
+        tmp = tmp.replace(/&/g, "&amp;");
+        tmp = tmp.replace(/</g, "&lt;");
+        tmp = tmp.replace(/>/g, "&gt;");
+        tmp = tmp.replace(/\n/g, "<br/>");
+        L.push(n.NUMBER + " [label=<" + tmp + ">];");
+    });
+    walk(root, function (n) {
+        if (!n || !n.children)
+            return;
+        n.children.forEach(function (x) {
+            if (!x)
+                return;
+            L.push(n.NUMBER + " -> " + x.NUMBER + ";");
+        });
+    });
+    L.push("}");
+    fs.writeFileSync(fname, L.join("\n"));
+}
+main();
